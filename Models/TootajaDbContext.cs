9f65454454bfea4893c49648afb041db33e8169d
MartinN <killcraft96@gmail.com>
2024-11-18 22:26:18 +0200

    Väikesed testid [#188474178]

--- Models/TootajaDbContext.cs
+++ Models/TootajaDbContext.cs
@@ -28,7 +28,8 @@ namespace API_XML_XSLT.Models
             {
                 entity.HasKey(e => e.Id);
                 entity.HasOne(e => e.Tootaja)
-                      .WithMany(t => t.Igapaeva_Andmed)
+                      //.WithMany(t => t.Igapaeva_Andmed)
+                      .WithMany()
                       .HasForeignKey(e => e.TootajaId)
                       .OnDelete(DeleteBehavior.Cascade);
             });
